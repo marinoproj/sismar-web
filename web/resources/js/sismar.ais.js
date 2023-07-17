44b5cb3e904551cb46c059dc95e171d4daeb2fdf
Leonardo Barbosa <leobar1995@gmail.com>
2023-07-16 21:15:39 -0300

    add cartao nautica

--- web/resources/js/sismar.ais.js
+++ web/resources/js/sismar.ais.js
@@ -737,7 +737,7 @@ Sismar.ais = function () {
         var nauticalChartSantos = new L.ImageOverlay('/sismar/faces/javax.faces.resource/img/new_santos-min.png',
                 [[-23.95000000, -46.39305556], [-24.08416667, -46.27750000]]);
         var proxPortoSaoSebastiao = new L.ImageOverlay('/sismar/faces/javax.faces.resource/img/proximidades_porto_sao_sebastiao.png',
-                [[-23.623765, -45.690765], [-24.120435, -45.008240]], {opacity: 0.7});
+                [[-23.621249, -45.753250], [-24.120435, -44.971161]], {opacity: 0.7});
         nauticalCharts = [{name: "Santos", layer: nauticalChartSantos},
                           {name: "Proximidades do porto - São Sebastião", layer: proxPortoSaoSebastiao}];
     };
